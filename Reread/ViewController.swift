dfeadea9f1206e74afbcd3f646f6bc6bca0979f2
近藤大翔 <taisho90@icloud.com>
2018-06-22 16:58:46 +0900

    1st commit

---
+++ Reread/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reread
+//
+//  Created by 近藤大翔 on 2018/06/22.
+//  Copyright © 2018年 近藤大翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
